68b8ce27440019893f8eef1797f9266ea3aba434
ljj <877966303@qq.com>
2017-09-09 19:47:14 +0800

    添加工厂模式注释

--- src/factory/DealAll.java
+++ src/factory/DealAll.java
@@ -1,5 +1,9 @@
 package factory;
 
+/**
+ * @author ljj
+ * info:工厂模式必须的接口
+ */
 public interface DealAll {
 public void dealThings();
 }
--- src/factory/DealTest.java
+++ src/factory/DealTest.java
@@ -1,6 +1,15 @@
 package factory;
 
+/**
+ * @author ljj
+ *
+ */
 public class DealTest {
+	
+/**测试工厂模式类
+ * @param type
+ * @return
+ */
 public static DealAll getbean(String type){
 	if("one".equals(type)){
 		return new ChildDealOne();
@@ -13,6 +22,7 @@ public static DealAll getbean(String type){
 	}
 }
 public static void main(String[] args){
+	//获取对应的对象
 	DealAll all = getbean("one");
 	all.dealThings();
 }
